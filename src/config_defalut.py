948399d8343dce3b240360c69e162fa9f466e7cd
南图 <vinthony@gmail.com>
2015-03-06 21:29:19 +0800

    add config

---
+++ src/config_defalut.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+configs = {
+	'db' : {
+		'host':127.0.0.1,
+		'port': 3306,
+		'user':'root',
+		'password':123456,
+		'database':'awesome'
+	},
+	'session':{
+		'secret':'AeEsOmE'
+	}
+}
