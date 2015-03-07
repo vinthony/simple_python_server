cbc5d486ef49beffc307e80410bd220f67a9b448
南图 <vinthony@gmail.com>
2015-03-08 02:08:07 +0800

    add front-end

--- src/urls.py
+++ src/urls.py
@@ -7,8 +7,9 @@ from transwarp.web import get,view
 
 from models import User,Blog,Comment
 
-@view('test_users.html')
+@view('blogs.html')
 @get('/')
 def test_users():
-	users = User.find_all()
-	return dict(users=users)
+	blogs = Blog.find_all()
+	user = User.find_first('where email=?','vinthony@gmail.com')
+	return dict(blogs=blogs,user=user)
--- src/wsgiapp.py
+++ src/wsgiapp.py
@@ -14,7 +14,18 @@ from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
 
 from config import configs
-
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 # init db:Template
 db.create_engine(**configs.db)
 
@@ -22,6 +33,7 @@ db.create_engine(**configs.db)
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
@@ -30,4 +42,6 @@ import urls
 wsgi.add_module(urls)
 
 if __name__ == '__main__':
-    wsgi.run(9000)
+    wsgi.run(9000, host='0.0.0.0')
+else:
+    application = wsgi.get_wsgi_application()
