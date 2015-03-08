e13cea593857d75aaaff2545b9a4a1336842aa18
南图 <vinthony@gmail.com>
2015-03-08 23:21:11 +0800

    add flat ui

---
+++ src/apis.py
@@ -0,0 +1,52 @@
+#!/usr/local/env python
+# -*- coding:utf-8 -*-
+
+import re, json, logging, functools
+from transwarp.web import ctx
+def dumps(obj):
+	return json.dumps(obj)
+class APIError(StandardError):
+    '''
+    the base APIError which contains error(required), data(optional) and message(optional).
+    '''
+    def __init__(self, error, data='', message=''):
+        super(APIError, self).__init__(message)
+        self.error = error
+        self.data = data
+        self.message = message
+
+class APIValueError(APIError):
+    '''
+    Indicate the input value has error or invalid. The data specifies the error field of input form.
+    '''
+    def __init__(self, field, message=''):
+        super(APIValueError, self).__init__('value:invalid', field, message)
+
+class APIResourceNotFoundError(APIError):
+    '''
+    Indicate the resource was not found. The data specifies the resource name.
+    '''
+    def __init__(self, field, message=''):
+        super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
+
+class APIPermissionError(APIError):
+    '''
+    Indicate the api has no permission.
+    '''
+    def __init__(self, message=''):
+        super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
+
+
+
+def api(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		try:
+			r = json.dumps(func(*args,**kw))
+		except APIError,e:
+			r = json.dumps(dict(error=e.error,data=e.data,message=e.message))
+		except Exception,e:
+			r = json.dumps(dict(error='internalerror',data=e.__class__.__name__,message=e.message))
+		ctx.response.contetn_type = 'application/json'			
+		return r
+	return _wrapper	
--- src/urls.py
+++ src/urls.py
@@ -1,15 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
+import logging,re
 
-from transwarp.web import get,view
+from transwarp.web import get,view,post,ctx
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User,Blog,Comment
 
+#@api
 @view('blogs.html')
 @get('/')
 def test_users():
 	blogs = Blog.find_all()
 	user = User.find_first('where email=?','vinthony@gmail.com')
 	return dict(blogs=blogs,user=user)
+
+_RE_MD5 = re.compile(r'^[0-9a-f]{32}$')
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+
+@api
+@post('/api/users')
+def register_user():
+    i = ctx.request.input(name='',email='',password='')
+    name = i.name.strip()
+    email = i.email.strip().lower()
+    password = i.password
+    if not name :
+    	raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+    	raise APIValueError('email')
+    if not password:
+    	raise APIValueError('password')
+    user = User.find_first('where email=?',email)
+    if user:
+    	raise APIError('register:failed','email','email is already used')
+    user = User(name=name,email=email,password=password,image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email).hexdigest())
+    user.insert()
+    return user				
+
+@view('register.html')
+@get('/register')
+def register():
+    return dict()
+
+@view('signin.html')
+@get('/signin')
+def signin():
+	return dict()
+
+@post('/api/authenticate')
+def signin_user():
+	pass
